5841713a8039c2d67b01362babb8481c2d17ebb3
Joel Degerman <joel@joeldegerman.se>
2020-04-28 14:55:48 +0200

    Added search for img

--- onStartup.js
+++ onStartup.js
@@ -5,15 +5,3 @@
 // }
 
 // loadLatestImages();
-
-const key = "VLPsY0ev6I5E85TnViUjU_QTdOW7gjL6cdinOxhNxRU";
-let pageCount = 1;
-let image = document.querySelector("img");
-
-function getListOfImages(){
-     fetch(`https://api.unsplash.com/photos/?client_id=${key}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
-
-getListOfImages();
