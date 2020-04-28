a4939f6bceb578137d9965a811b297fc435995cf
Erik Lindh <erik@tymelapse.com>
2020-04-28 15:40:37 +0200

    laddar in senaste bilderna - download fungerar ej

--- onStartup.js
+++ onStartup.js
@@ -1,19 +1,32 @@
-// async function loadLatestImages(){
-//     let response = await fetch("https://api.unsplash.com/photos/random");
-//     let data = await response.json();
-//     console.log(data);
-// }
-
-// loadLatestImages();
-
 const key = "VLPsY0ev6I5E85TnViUjU_QTdOW7gjL6cdinOxhNxRU";
+const sortOrder = "latest";
 let pageCount = 1;
-let image = document.querySelector("img");
 
-function getListOfImages(){
-     fetch(`https://api.unsplash.com/photos/?client_id=${key}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+let imageArray = document.querySelectorAll("img");
+
+async function fillImagesAtStartup(){
+    let response = await fetch(`https://api.unsplash.com/photos/?page=${pageCount}&order_by=${sortOrder}&client_id=${key}`);
+    let data = await response.json();
+
+    console.log(data);
+
+    for(let i = 0; i < data.length; i++){
+        imageArray[i].addEventListener("click", downloadImage);
+        imageArray[i].id = data[i].links.download;
+        imageArray[i].setAttribute("src", data[i].urls.small);
+        imageArray[i].setAttribute("alt", data[i].alt_description);
+    }
 }
 
-getListOfImages();
+//fungerar ej - TODO fixa
+function downloadImage(){
+    let linkCreated =  document.createElement("a");
+
+    //event.target.id innehåller download-länken
+    linkCreated.setAttribute("href", event.target.id);
+    linkCreated.setAttribute("download", event.target.id);
+    linkCreated.click();
+    linkCreated.parentNode.removeChild(linkCreated);
+}
+
+fillImagesAtStartup();
