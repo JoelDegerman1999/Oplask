9c2ab4950e1276671a9da83d72ccd553dd789904
Joel Degerman <joel@joeldegerman.se>
2020-04-30 15:12:27 +0200

    refactor

--- js/onStartup.js
+++ js/onStartup.js
@@ -1,96 +1,86 @@
-const key = "VLPsY0ev6I5E85TnViUjU_QTdOW7gjL6cdinOxhNxRU";
+const erik_key = "VLPsY0ev6I5E85TnViUjU_QTdOW7gjL6cdinOxhNxRU";
+const joel_key = "qivyMbeL3252shGCXge88F3-1WyvLC3n1Gzf40YG84c";
+
 const sortOrder = "latest";
 let pageCount = 1;
-
+let maxPage;
 const articlePhoto = document.querySelector(".photo");
 const container = document.querySelector(".gallery");
 let downloadedImages = [];
-let favoriteImages = JSON.parse(localStorage.getItem("favoriteImages"));
 
-if(favoriteImages == null) favoriteImages = []
+let favoriteImages = JSON.parse(localStorage.getItem("favoriteImages"));
+if (favoriteImages == null) favoriteImages = [];
 
 async function loadNewestImagesAtStartup() {
   let response = await fetch(
-    `https://api.unsplash.com/photos/?page=${pageCount}&order_by=${sortOrder}&client_id=${key}&per_page=20`
+    `https://api.unsplash.com/photos/?page=${pageCount}&order_by=${sortOrder}&client_id=${erik_key}&per_page=20`
   );
   let data = await response.json();
 
-  for (let i = 0; i < data.length; i++) {
-    downloadedImages.push(data[i]);
+  data.map((photo) => {
+    downloadedImages.push(photo);
     let newArticle = articlePhoto.cloneNode(true);
 
     let anchorTag = document.createElement("a");
-    anchorTag.setAttribute("href", data[i].urls.full);
+    anchorTag.setAttribute("href", photo.urls.full);
     anchorTag.setAttribute("data-lightbox", "mygallery");
-    anchorTag.setAttribute("data-title", data[i].user.name);
+    anchorTag.setAttribute("data-title", photo.user.name);
     newArticle.appendChild(anchorTag);
 
     newArticle.classList.remove("prototype");
     let img = newArticle.querySelector(".img-photo");
-    img.setAttribute("alt", data[i].alt_description);
-    img.setAttribute("src", data[i].urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("src", photo.urls.regular);
 
     let favoriteBtn = newArticle.querySelector(".favorite");
-    favoriteBtn.addEventListener("click", () => {
-      if (favoriteBtn.src == "http://127.0.0.1:5500/img/full-like.png") {
-        favoriteBtn.src = "http://127.0.0.1:5500/img/like.png";
-      } else {
-        favoriteBtn.src = "http://127.0.0.1:5500/img/full-like.png";
-      }
-      markAsFavorite();
-    });
+    favoriteBtn.addEventListener("click", () => markAsFavorite());
 
     anchorTag.appendChild(img);
     container.appendChild(newArticle);
-  }
-}
+  });
 
-//TODO fixa - fungerar ej i nyare Chrome-versioner
-async function downloadImage() {
-  let url = event.target.id;
-  let response = await fetch(`${url}/?client_id=${key}`);
-  let data = await response.json();
-  let downloadLink = data.url;
-
-  let linkCreated = document.createElement("a");
-  linkCreated.setAttribute("href", downloadLink);
-  linkCreated.setAttribute("download", "output");
-
-  document.body.appendChild(linkCreated);
-  linkCreated.click();
-  document.body.removeChild(linkCreated);
+  let allImg = document.querySelectorAll(".favorite");
+  favoriteImages.forEach((favorite) => {
+    for (let i = 0; i < downloadedImages.length; i++) {
+      if (favorite.id == downloadedImages[i].id) {
+        allImg[i + 1].src = "http://127.0.0.1:5500/img/full-like.png";
+      }
+    }
+  });
 }
 
 function markAsFavorite() {
-
+  let isSame = false;
   let article = event.target.parentNode;
   let img = article.querySelector(".img-photo");
-
   let urlToLookFor = img.src;
-
   for (let i = 0; i < downloadedImages.length; i++) {
     if (downloadedImages[i].urls.regular == urlToLookFor) {
-      console.log("utanför unfavo")
-      if (favoriteImages.includes(downloadedImages[i])) {
-        console.log("unfavo")
-        //UN-FAVORITE
-        favoriteImages.splice(favoriteImages.indexOf(downloadedImages[i]), 1);
-        console.log(favoriteImages);
-        localStorage.setItem("favoriteImages", JSON.stringify(favoriteImages));
-        return;
+      favoriteImages.forEach((element) => {
+        if (element.id == downloadedImages[i].id) {
+          isSame = true;
+        }
+      });
+      if (isSame) {
+        if (returnWhichHtmlSite() == "favorites.html")
+          favoriteImages.splice(i, 1);
+      } else {
+        favoriteImages.push(downloadedImages[i]);
       }
-      //FAVORITE
-      favoriteImages.push(downloadedImages[i]);
-      console.log(favoriteImages);
+      let favoriteImg = event.target.parentNode.querySelector(".favorite");
+      if (favoriteImg.src == "http://127.0.0.1:5500/img/like.png")
+        favoriteImg.src = "http://127.0.0.1:5500/img/full-like.png";
     }
   }
-
   localStorage.setItem("favoriteImages", JSON.stringify(favoriteImages));
+
+  if (returnWhichHtmlSite() == "favorites.html") {
+    location.reload();
+  }
 }
 
 function loadFavoritesOnStartup() {
-  let favorites = JSON.parse(localStorage.getItem("favoriteImages"));
-  if (favorites == null) favorites = [];
+  let favorites = favoriteImages;
 
   let clearStorageBtn = document.querySelector(".clear-storage");
 
@@ -120,25 +110,12 @@ function loadFavoritesOnStartup() {
     anchorTag.appendChild(img);
     container.appendChild(newArticle);
   }
+
+  let likeSymbols = document.querySelectorAll(".favorite");
+  likeSymbols.forEach((element) => {
+    element.src = "http://127.0.0.1:5500/img/full-like.png";
+  });
 }
-
-let path = window.location.pathname;
-let page = path.split("/").pop();
-if (page == "index.html") {
-  loadNewestImagesAtStartup();
-  console.log(favoriteImages);
-} else if (page == "favorites.html") {
-  console.log(favoriteImages);
-  loadFavoritesOnStartup();
-}
-
-/*script.js                                  */
-
-const access_key = "qivyMbeL3252shGCXge88F3-1WyvLC3n1Gzf40YG84c";
-let page_count = 1;
-let maxPage;
-const article_photo = document.querySelector(".photo");
-const containerGallery = document.querySelector(".gallery");
 
 async function Main() {
   let input = document.querySelector(".search-input");
@@ -163,53 +140,53 @@ async function Main() {
 
 async function searchForImage(input) {
   let rawData = await fetch(
-    `https://api.unsplash.com/search/photos/?query=${input}&client_id=${access_key}&per_page=20`
+    `https://api.unsplash.com/search/photos/?query=${input}&client_id=${joel_key}&per_page=20`
   );
   let data = await rawData.json();
   return data;
 }
 
 async function showSearchedImage(input, callback) {
-  containerGallery.innerHTML = "";
+  container.innerHTML = "";
   downloadedImages = [];
   let data = await callback(input);
-  console.log(data);
-  if (data != null) {
-    let photoData = data.results;
-    photoData.map((photo) => {
-      downloadedImages.push(photo);
-      let anchorTag = document.createElement("a");
-      anchorTag.setAttribute("href", photo.urls.full);
-      anchorTag.setAttribute("data-lightbox", "mygallery");
-      anchorTag.setAttribute("data-title", photo.user.name);
+  if (data == null) return;
 
-      let newArticle = article_photo.cloneNode(true);
-      newArticle.classList.remove("prototype");
-      let img = newArticle.querySelector(".img-photo");
-      img.setAttribute("src", photo.urls.regular);
+  data.results.map((photo) => {
+    downloadedImages.push(photo);
+    let anchorTag = document.createElement("a");
+    anchorTag.setAttribute("href", photo.urls.full);
+    anchorTag.setAttribute("data-lightbox", "mygallery");
+    anchorTag.setAttribute("data-title", photo.user.name);
 
-      let favoriteBtn = newArticle.querySelector(".favorite");
-      favoriteBtn.addEventListener("click", markAsFavorite);
+    let newArticle = articlePhoto.cloneNode(true);
+    newArticle.classList.remove("prototype");
+    let img = newArticle.querySelector(".img-photo");
+    img.setAttribute("src", photo.urls.regular);
 
-      newArticle.appendChild(anchorTag);
-      anchorTag.appendChild(img);
-      containerGallery.appendChild(newArticle);
+    let favoriteBtn = newArticle.querySelector(".favorite");
+    favoriteBtn.addEventListener("click", () => {
+      markAsFavorite();
     });
-  }
+
+    newArticle.appendChild(anchorTag);
+    anchorTag.appendChild(img);
+    container.appendChild(newArticle);
+  });
 }
 
 async function nextPage(input) {
-  page_count++;
-  if (page_count !== maxPage) {
+  pageCount++;
+  if (pageCount !== maxPage) {
     let rawData = await fetch(
-      `https://api.unsplash.com/search/photos/?query=${input}&client_id=${access_key}&per_page=20&page=${page_count}`
+      `https://api.unsplash.com/search/photos/?query=${input}&client_id=${joel_key}&per_page=20&page=${pageCount}`
     );
     let data = await rawData.json();
     maxPage = data.total_pages;
     return data;
-  } else if (page_count == maxPage) {
+  } else if (pageCount == maxPage) {
     rawData = await fetch(
-      `https://api.unsplash.com/search/photos/?query=${input}&client_id=${access_key}&per_page=20&page=${maxPage}`
+      `https://api.unsplash.com/search/photos/?query=${input}&client_id=${joel_key}&per_page=20&page=${maxPage}`
     );
     data = await rawData.json();
     return data;
@@ -217,22 +194,31 @@ async function nextPage(input) {
 }
 
 async function prevPage(input) {
-  page_count--;
-  if (page_count > 1) {
-    console.log(page_count);
+  pageCount--;
+  if (pageCount > 1) {
+    console.log(pageCount);
     let rawData = await fetch(
-      `https://api.unsplash.com/search/photos/?query=${input}&client_id=${access_key}&per_page=20&page=${page_count}`
+      `https://api.unsplash.com/search/photos/?query=${input}&client_id=${joel_key}&per_page=20&page=${pageCount}`
     );
     let data = await rawData.json();
     return data;
   } else {
-    page_count = 1;
+    pageCount = 1;
     let rawData = await fetch(
-      `https://api.unsplash.com/search/photos/?query=${input}&client_id=${access_key}&per_page=20&page=1`
+      `https://api.unsplash.com/search/photos/?query=${input}&client_id=${joel_key}&per_page=20&page=${pageCount}`
     );
     let data = await rawData.json();
     return data;
   }
 }
 
+function returnWhichHtmlSite() {
+  return window.location.pathname.split("/").pop();
+}
+
+if (returnWhichHtmlSite() == "index.html") {
+  loadNewestImagesAtStartup();
+} else if (returnWhichHtmlSite() == "favorites.html") {
+  loadFavoritesOnStartup();
+}
 Main();
